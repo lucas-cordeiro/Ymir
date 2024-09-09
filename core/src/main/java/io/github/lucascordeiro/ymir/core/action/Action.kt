1a8ceabf03ac15d4bb134e948e8160e6612ba059
Lucas Cordeiro <lucas-gabriel-2000@hotmail.com>
2024-09-09 17:00:00 -0300

    fix: action lifecycle observe

--- core/src/main/java/io/github/lucascordeiro/ymir/core/action/Action.kt
+++ core/src/main/java/io/github/lucascordeiro/ymir/core/action/Action.kt
@@ -1,6 +1,8 @@
 package io.github.lucascordeiro.ymir.core.action
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.receiveAsFlow
 
 class Action<Action: UiAction> {
--- core/src/main/java/io/github/lucascordeiro/ymir/core/utils/LifecycleUtils.kt
+++ core/src/main/java/io/github/lucascordeiro/ymir/core/utils/LifecycleUtils.kt
@@ -2,9 +2,16 @@ package io.github.lucascordeiro.ymir.core.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import io.github.lucascordeiro.ymir.core.action.UiAction
 import io.github.lucascordeiro.ymir.core.state.UiState
 import io.github.lucascordeiro.ymir.core.viewmodel.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
 
 object LifecycleUtils {
     @Composable
@@ -12,8 +19,14 @@ object LifecycleUtils {
         viewModel: T,
         handleAction: (A) -> Unit
     ) {
-        LaunchedEffect(Unit) {
-            viewModel.action.collect(handleAction)
+        val lifecycle = LocalLifecycleOwner.current.lifecycle
+
+        LaunchedEffect(viewModel, lifecycle)  {
+            lifecycle.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+                withContext(Dispatchers.Main.immediate) {
+                    viewModel.action.collect(handleAction)
+                }
+            }
         }
     }
 }
